56b70a81708caafe55c6616f8d434d2f9f79b16b
Samuel <samnmbuguah@gmail.com>
2023-05-16 12:32:45 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+ def greet_programmer
+    puts "Hello, programmer!"
+ end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+
+def add(a, b)
+  a + b
+end
+
+def halve(number)
+  if number.is_a? Integer
+    number / 2
+  else
+    nil
+  end
+end
